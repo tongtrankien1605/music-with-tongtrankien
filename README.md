138875303f1bb03fd14be716ad3a8ad022a3e666
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-07-10 23:45:03 +0700

    Upload Code Base music-with-tongtrankien on 10/07/2025 by Tống Trần Kiên

---
+++ README.md
@@ -0,0 +1,5 @@
+## 🚀  **Thưởng thức âm nhạc cùng Tống Trần Kiên** 🚀
+
+<br/>
+
+## 🌐  **👉  Nghe & thư giãn [TẠI ĐÂY](https://tongtrankien1605.github.io/music-with-tongtrankien/) nhé cả nhà 👈**
---
+++ songs.js
@@ -0,0 +1,58 @@
+// songs.js
+const songs = [
+    {
+        title: "Chơi Như Tụi Mỹ",
+        artist: "Andree Right Hand",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/choi-nhu-tui-my-andreerighthand.mp3"
+    },
+    {
+        title: "Bao Giờ Lấy Vợ",
+        artist: "24K Right",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/bao-gio-lay-vo-rapviet-24kright.mp3"
+    },
+    {
+        title: "Ba Ơi",
+        artist: "Robber",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/ba-oi-rapviet-robber.mp3"
+    },
+    {
+        title: "Đổi Tư Thế (Remix)",
+        artist: "Andree Right Hand, BinhGold",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/doi-tu-the-remix-binhgold-andreerighthand.mp3"
+    },
+    {
+        title: "Hoa Hồng Gai",
+        artist: "Pháp Kiều",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/hoa-hong-gai-rapviet-phapkieu.mp3"
+    },
+    {
+        title: "Hơi Bị Nghệ",
+        artist: "7Dnight",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/hoi-bi-nghe-rapviet-7dnight.mp3"
+    },
+    {
+        title: "Hot Shot",
+        artist: "Dlow",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/hot-shot-rapviet-dlow.mp3"
+    },
+    {
+        title: "Huỳnh Kim Long",
+        artist: "Huỳnh Công Hiếu",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/huynh-kim-long-rapviet-huynhconghieu.mp3"
+    },
+    {
+        title: "Khi Cơn Mưa Dần Phai",
+        artist: "Tez",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/khi-con-mua-dan-phai-rapviet-tez.mp3"
+    },
+    {
+        title: "Qua Từng Khung Hình",
+        artist: "Robber, Ngắn",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/qua-tung-khung-hinh-rapviet-robber-ngan.mp3"
+    },
+    {
+        title: "Take It Easy",
+        artist: "Liu Grace",
+        src: "https://cdn.jsdelivr.net/gh/tongtrankien1605/music-with-tongtrankien@main/music/take-it-easy-rapviet-liugrace.mp3"
+    }
+];
